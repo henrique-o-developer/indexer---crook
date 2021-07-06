81c05bc85747e3d21bb27beb05fbb94b769040fd
fenisosi <henriquefrancheso50@gmail.com>
2021-07-06 17:27:05 -0300

    #5

--- cloud/index.js
+++ cloud/index.js
@@ -1,1 +1,16 @@
-console.log("i'm running")
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+
+app.get("/ram", async(req, res) => {
+    try {
+        res.send("a'm running");
+    } catch(err) {
+        delete err
+    }
+})
+
+
+server.listen(3000);
